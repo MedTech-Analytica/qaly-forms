a0be26f8df2f2b9ae89b74d597c0b044d505f5c0
tivie <estevao.santos@gmail.com>
2021-09-15 11:02:22 +0100

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+jQuery(function ($) {
+
+  //var foo = $.getJSON("inquerito/inquerito.json");
+  var foo = $.get("inquerito/inquerito.yml");
+  
+  foo.done(function(inquerito) {
+
+    var json = jsyaml.load(inquerito);
+    
+    Survey
+      .StylesManager
+      .applyTheme("modern");
+
+    window.survey = new Survey.Model(json);
+
+    survey
+      .onComplete
+      .add(function (sender) {
+        document
+          .querySelector('#surveyResult')
+          .textContent = "Result JSON:\n" + JSON.stringify(sender.data, null, 3);
+      });
+
+    $("#surveyElement").Survey({model: survey});
+  });
+  
+});
+
+
+
