40cfebf9acd34352a3d9b7c313c5ba8f6acf63dc
tivie <estevao.santos@gmail.com>
2021-09-16 21:09:18 +0100

    add localization

--- src/js/app.js
+++ src/js/app.js
@@ -6,11 +6,11 @@ jQuery(function ($) {
   foo.done(function(inquerito) {
 
     var json = jsyaml.load(inquerito);
-    
+
     Survey
       .StylesManager
       .applyTheme("modern");
-
+    
     window.survey = new Survey.Model(json);
 
     survey
@@ -21,6 +21,7 @@ jQuery(function ($) {
           .textContent = "Result JSON:\n" + JSON.stringify(sender.data, null, 3);
       });
 
+    survey.locale = 'pt';
     $("#surveyElement").Survey({model: survey});
   });
   
